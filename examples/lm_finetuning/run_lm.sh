2760c2c2f7c6313a5011760a91b7e895048bfead
Louis <ljean@etud.insa-toulouse.fr>
2019-09-03 10:49:48 +0200

    Try to match dede workflow for debug purpose

--- examples/lm_finetuning/run_lm.sh
+++ examples/lm_finetuning/run_lm.sh
@@ -1,9 +1,9 @@
 rm finetuning_lm2/*
-python3 simple_lm_finetuning.py \
+python3 custom_lm_finetuning.py \
+    --bert_model /home/louisj/pretrained/bert/ \
     --train_corpus /home/louisj/dede/data/agnews_subset/train/news/data.txt \
-    --test_corpus /home/louisj/dede/data/agnews_subset/test/news/data.txt \
+    --test_corpus /home/louisj/dede/data/agnews_subset/same_x12/news/data.txt \
     --learning_rate 1e-5 \
-    --bert_model bert-base-uncased \
     --num_train_epochs 0 \
     --do_lower_case \
     --output_dir finetuning_lm2/ \
